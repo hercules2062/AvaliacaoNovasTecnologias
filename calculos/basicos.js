1069076244380aacfefd8c721e5b7870af14c03e
Arthur V. M. Dantas <arthur.dantas@ifba.edu.br>
2024-08-20 20:42:05 -0300

    adicionada função para somar números
    a função somarNumeros pode receber um número arbitrário de parâmetros e retorna o valor da soma desses parâmetros

--- calculos/basicos.js
+++ calculos/basicos.js
@@ -1,1 +1,8 @@
-// AQUI SERÃO IMPLEMENTADAS AS FUNÇÕES BÁSICAS DE CALCULO
+/**
+ * Soma valores numéricos
+ * @param  {...number} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarNumeros(...numeros) {
+  return numeros.reduce((total, proximo) => total + proximo, 0);
+}
