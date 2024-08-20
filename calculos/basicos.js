4e9518d827dd4ad382929462939fc6c2d1f8024a
Arthur V. M. Dantas <arthur.dantas@ifba.edu.br>
2024-08-20 20:48:07 -0300

    adicionada função para somar strings
    a função somarStrings pode receber um número arbitrário de parâmetros e retorna o valor da soma desses parâmetros (após conversão para números)

--- calculos/basicos.js
+++ calculos/basicos.js
@@ -15,3 +15,13 @@ export function somarNumeros(...numeros) {
 export function somarNumerosArray(numeros) {
   return numeros.reduce((total, proximo) => total + proximo, 0);
 }
+
+/**
+ * Soma, como valores numéricos, valores em strings
+ * @param  {...string} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarStrings(...numeros) {
+  const valores = numeros.map((n) => Number(n));
+  return somarNumeros(valores);
+}
