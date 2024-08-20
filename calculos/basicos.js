8108e0b7378663d2e9f4d2c27b11417cf48e384a
Arthur V. M. Dantas <arthur.dantas@ifba.edu.br>
2024-08-20 20:50:26 -0300

    adicionada função para somar itens (strings) em um array
    a função somarStringsArray recebe um array de strings e retorna a soma de todos os itens do array após convertê-los para números

--- calculos/basicos.js
+++ calculos/basicos.js
@@ -25,3 +25,13 @@ export function somarStrings(...numeros) {
   const valores = numeros.map((n) => Number(n));
   return somarNumeros(valores);
 }
+
+/**
+ * Soma, como valores numéricos, valores presentes em um array de strings
+ * @param {string} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarStringsArray(numeros) {
+  const valores = numeros.map((n) => Number(n));
+  return somarNumerosArray(valores);
+}
