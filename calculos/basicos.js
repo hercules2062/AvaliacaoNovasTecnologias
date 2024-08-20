1a4dac4118fe0af9f6e31531a3d719885c18ed68
Arthur V. M. Dantas <arthur.dantas@ifba.edu.br>
2024-08-20 20:44:18 -0300

    adicionada função para somar itens (números) em um array
    a função somarNumerosArray recebe um array de números e retorna a soma de todos os itens do array

--- calculos/basicos.js
+++ calculos/basicos.js
@@ -6,3 +6,12 @@
 export function somarNumeros(...numeros) {
   return numeros.reduce((total, proximo) => total + proximo, 0);
 }
+
+/**
+ * Soma valores numéricos presentes em um array
+ * @param {number} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarNumerosArray(numeros) {
+  return numeros.reduce((total, proximo) => total + proximo, 0);
+}
