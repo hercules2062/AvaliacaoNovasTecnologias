d27c4f4184d151e1a21c8e4515304ed7209e48e3
Hercules Junior <202311240006@ifba.edu.br>
2024-08-30 21:36:47 -0300

    bug corrigido

--- calculos/basicos.js
+++ calculos/basicos.js
@@ -23,6 +23,16 @@ export function somarNumerosArray(numeros) {
  */
 export function somarStrings(...numeros) {
   const valores = numeros.map((n) => Number(n));
+  return somarNumerosArray(valores);
+}
+
+/**
+ * Soma, como valores numéricos, valores em strings
+ * @param  {...string} numeros Valores a serem somados
+ * @returns Total da soma
+ */
+export function somarStrings(...numeros) {
+  const valores = numeros.map((n) => Number(n));
   return somarNumeros(valores);
 }
 
